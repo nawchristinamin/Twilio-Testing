94b6951d6f32f51f896dd4faea9244e140ff17a0
nawchristinamin <nawchristinamin@fellowship-corp.com>
2025-08-19 17:29:49 +0900

    add key

--- send_sms.py
+++ send_sms.py
@@ -3,9 +3,9 @@ from twilio.rest import Client
 # Your Twilio Account SID and Auth Token from https://console.twilio.com/
 account_sid = "AC83f5064c6436526612639e5206eac1e4"
 auth_token = "c6cdd1aa0c7521542e4474f26e592149"
-msg_service_sid = "MGc1f422505413a6da99d214023a77c8bb"  # starts with "MG"
+msg_service_sid = "MGa4c3d061bd46c742040bb66c2a0f6af1"  # starts with "MG"
 api_key_sid = "SK31ba400dea33c2777f8ab5a31d0e30ce"   # From Console
-api_key_secret = "LQW0RHmF2zr8L0GRRgpBEr7bsSsMsPma"    
+api_key_secret = "LQW0RHmF2zr8L0GRRgpBEr7bsSsMsPma"  
 
 client = Client(api_key_sid, api_key_secret, account_sid)
 
